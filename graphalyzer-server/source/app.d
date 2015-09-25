68665a8772a73412eaa7ec7772ae4fef53768be5
Richard White <rwhite226@gmail.com>
2015-09-25 12:47:31 -0500

    actually made it recieve a post

--- graphalyzer-server/source/app.d
+++ graphalyzer-server/source/app.d
@@ -1,16 +1,19 @@
-import vibe.d;
+import std.stdio, std.datetime, vibe.d;
 
-shared static this()
-{
-	auto settings = new HTTPServerSettings;
-	settings.port = 8080;
-	settings.bindAddresses = ["::1", "127.0.0.1"];
-	listenHTTP(settings, &hello);
-
-	logInfo("Please open http://127.0.0.1:8080/ in your browser.");
+void postFile(HTTPServerRequest req, HTTPServerResponse res) {
+    logInfo("%s: Recieved a POST from %s", Clock.currTime.toISOExtString(), req.peer);
+    string str = req.toString();
+    str = urlDecode(str[17 .. str.length-9]);
+    res.writeBody("Sucess");
 }
 
-void hello(HTTPServerRequest req, HTTPServerResponse res)
-{
-	res.writeBody("Hello, World!");
+shared static this() {
+	URLRouter router = new URLRouter;
+	router.post("/filesubmit/*", &postFile);
+
+    HTTPServerSettings settings = new HTTPServerSettings;
+	settings.port = 8080;
+	settings.bindAddresses = ["::1", "127.0.0.1"];
+
+	listenHTTP(settings, router);
 }
