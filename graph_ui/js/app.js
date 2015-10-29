483358db84398e434a2cfd4af2f06969720a6dae
Andrew Bowler <ajbowler@iastate.edu>
2015-10-28 21:12:05 -0500

    Add temporary basic css for panel section clarity

--- graph_ui/js/app.js
+++ graph_ui/js/app.js
@@ -7,31 +7,8 @@
 
   app.controller('GraphController', ['$scope', 'VisDataSet',
     function($scope, VisDataSet) {
-
-      $scope.onSelect = function(items) {
-        // debugger;
-        alert('select');
-      };
-
-      $scope.onClick = function(props) {
-        //debugger;
-        alert('Click');
-      };
-
-      $scope.onDoubleClick = function(props) {
-        // debugger;
-        alert('DoubleClick');
-      };
-
-      $scope.rightClick = function(props) {
-        alert('Right click!');
-        props.event.preventDefault();
-      };
-      
       $scope.options = {
-        autoResize: true,
-        height: '800',
-        width: '100%'
+        autoResize: true
       };
       
       $scope.data = {
