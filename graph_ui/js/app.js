e036e1f263a84e0b6ea2334b9eb1e1a2c0e38015
Andrew Bowler <ajbowler@iastate.edu>
2015-10-29 16:12:08 -0500

    Some code style cleanup

--- graph_ui/js/app.js
+++ graph_ui/js/app.js
@@ -3,31 +3,34 @@
  */
 
 (function() {
-  var app = angular.module('graphalyzer', ['ngVis']);
+  'use strict';
+  angular
+    .module('graphalyzer', ['ngVis'])
+    .controller('GraphController', GraphController);
 
-  app.controller('GraphController', ['$scope', 'VisDataSet',
-    function($scope, VisDataSet) {
-      $scope.options = {
-        autoResize: true
-      };
-      
-      $scope.data = {
-        "nodes": 
-        [
-          {id: 1, label: 'Node 1'},
-          {id: 2, label: 'Node 2'},
-          {id: 3, label: 'Node 3'},
-          {id: 4, label: 'Node 4'},
-          {id: 5, label: 'Node 5'}
-        ],
-        "edges":
-        [
-          {from: 1, to: 3},
-          {from: 1, to: 2},
-          {from: 2, to: 4},
-          {from: 2, to: 5}
-        ]
-      }
+  GraphController.$inject['$scope', 'VisDataSet'];
+
+  function GraphController($scope, VisDataSet) {
+    $scope.options = {
+      autoResize: true
+    };
+    
+    $scope.data = {
+      "nodes": 
+      [
+        {id: 1, label: 'Node 1'},
+        {id: 2, label: 'Node 2'},
+        {id: 3, label: 'Node 3'},
+        {id: 4, label: 'Node 4'},
+        {id: 5, label: 'Node 5'}
+      ],
+      "edges":
+      [
+        {from: 1, to: 3},
+        {from: 1, to: 2},
+        {from: 2, to: 4},
+        {from: 2, to: 5}
+      ]
     }
-  ]);
+  }
 })();
