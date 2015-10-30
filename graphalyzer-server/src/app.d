73821e8f32bdedaa2879635e362a5fd5e73eefd7
Richard White <rwhite226@gmail.com>
2015-10-30 17:18:46 -0500

    Added a simple REST server and a web page to upload files.

--- graphalyzer-server/src/app.d
+++ graphalyzer-server/src/app.d
@@ -1,6 +1,15 @@
 module first;
 import vibe.d;
 
+/*
+* Here wll be the config options parsed from some config file
+* They are hard coded for now.
+*/
+
+immutable ushort PORT;
+immutable int MAX_REST_SIZE;
+immutable string UPLOAD_LOCATION;
+
 /************************************************
  * Handles Main entrypoint for server as opposed to void main()
  * Authors: Richard White, rwhite22@iastate.edu
@@ -10,11 +19,51 @@ shared static this() {
 
     import vibe.http.router, WebSocketServer.Server;
 
+    /*
+    * Here wll be the config options parsed from some config file
+    * They are hard coded for now.
+    */
+
+    PORT = 1618;
+    MAX_REST_SIZE = 1 * 1024 * 1024 * 1024;
+    UPLOAD_LOCATION = "./dir";
+
     auto router = new URLRouter;
+
+    //TEST
+    router.get("/test", staticTemplate!"upload_form.dt");
+
+    // Websocket Server
     router.get("/ws", handleWebSockets(&handleWebsocket!WebSocket));
 
+    // REST
+    router.post("/upload", &uploadFile);
+
     auto settings = new HTTPServerSettings;
-    settings.port = 1618;
+
+    // Port running on
+    settings.port = PORT;
+
+    // Max request size in bytes
+    settings.maxRequestSize = MAX_REST_SIZE;
     listenHTTP(settings, router);
 
+}
+
+/************************************************
+ * Handles the revieving of uploaded files via REST to UPLOAD_LOCATION.
+ * Authors: Richard White, rwhite22@iastate.edu
+ * Date: October 30, 2015
+ ***********************************************/
+void uploadFile(scope HTTPServerRequest req, scope HTTPServerResponse res) {
+    foreach (pf; req.files) {
+        try
+            moveFile(pf.tempPath, Path(UPLOAD_LOCATION) ~ pf.filename);
+        catch (Exception e) {
+            logWarn("Failed to move file to destination folder: %s", e.msg);
+            logInfo("Performing copy+delete instead.");
+            copyFile(pf.tempPath, Path(UPLOAD_LOCATION) ~ pf.filename);
+        }
+    }
+    res.writeBody("File uploaded!", "text/plain");
 }
