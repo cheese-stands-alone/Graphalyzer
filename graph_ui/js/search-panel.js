13982d5b005490175bf06afd2b3f0fd75a8e1b9d
Andrew Bowler <ajbowler@iastate.edu>
2015-11-02 19:06:20 -0600

    WIP attempting to connect to the server through WebSocket

--- graph_ui/js/search-panel.js
+++ graph_ui/js/search-panel.js
@@ -6,10 +6,24 @@
     .directive('searchPanel', function() {
       return {
         restrict: 'E',
+        scope: {searchQuery: '='},
         templateUrl: 'search-panel.html',
-        controller: function() {
-          this.search = function() {
-            console.log('Search!!!');
+        controller: function($scope) {
+          this.search = function() {          
+            var conn = new WebSocket("ws://rwhite226.duckdns.org:1618/ws");
+
+            var request = {  
+              "message_id": "",
+              "sender_id": "",
+              "time": "",
+              "request": "getgraph",
+              "status": "",
+              "error": "",
+              "payload": $scope.query.graphName,
+              "message": ""
+            };
+
+            conn.send(JSON.stringify(request));
           }
         },
         controllerAs: 'graphSearchHandler'
