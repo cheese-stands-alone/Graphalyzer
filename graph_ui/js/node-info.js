51b16d31542ffcc0118a35157314e9aeaddbf469
Andrew Bowler <ajbowler@iastate.edu>
2015-11-05 17:24:06 -0600

    Node Panel Directive cleanup

--- graph_ui/js/node-info.js
+++ graph_ui/js/node-info.js
@@ -6,53 +6,18 @@
 (function() {
   'use strict';
   angular
-    .module('nodeProperties', ['ngVis'])
-    .directive('nodePanel', nodePanel)
-    .controller('NodeInfoController', NodeInfoController);
-
-    function nodePanel() {
+    .module('nodeProperties', [])
+    .directive('nodePanel', function() {
       return {
         restrict: 'E',
         scope: {
           selectedNode: '='
         },
         templateUrl: 'node-panel.html',
-        controller: NodeInfoController,
+        controller: function($scope, selectedNodeService) {
+
+        },
         controllerAs: 'NodePanelController'
       };
-    }
-
-    NodeInfoController.$inject['$scope', 'VisDataSet'];
-
-    function NodeInfoController($scope)
-    {
-      var nodeInfo = [];
-      // However this vis selectNode function is invoked
-      node = network.on('selectNode', onSelectNode);
-      network.on('deselectNode', onDeselectNode);
-      for(var key in node)
-      {
-        if(node.hasOwnProperty(key))
-        {
-          nodeInfo.push(key);
-          nodeInfo.push(node[key]);
-        }
-        // This structures the array as {property1, entry1, property2, entry2 ... propertyN, entryN}
-      }
-    }
-
-    function onSelectNode(nodeInfo)
-    {
-      // Sanity check, REMOVE
-      alert("Did you select a node?");
-      // Push the HTML to list the information, RE: nodeInfo array
-    }
-
-    function onDeselectNode()
-    {
-      // Sanity check, REMOVE
-      alert("Did you deselect a node?");
-      // Remove the HTML to list the information, get ready for them to click another node
-      // Possibly resize the window pane
-    }
+    });
 })();
