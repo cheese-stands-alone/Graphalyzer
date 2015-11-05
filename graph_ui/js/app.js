f9db6736aa2b42ea00f1c65c4b15691d1fe1ba0f
Andrew Bowler <ajbowler@iastate.edu>
2015-11-05 17:24:40 -0600

    Selected Node service to store the selected node from the graph

--- graph_ui/js/app.js
+++ graph_ui/js/app.js
@@ -7,7 +7,52 @@
   'use strict';
   angular
     .module('graphalyzer', ['ngVis', 'searchDirective', 'nodeProperties'])
-    .controller('GraphController', GraphController)
+    .service('selectedNodeService', function() {
+      var selectedNode = {};
+
+      return {
+        getSelectedNode: function() {
+          return selectedNode;
+        },
+
+        setSelectedNode: function(newNode) {
+          selectedNode = newNode;
+        }
+      };
+    })
+    .controller('GraphController', ['$scope', 'VisDataSet', 'selectedNodeService', function($scope, VisDataSet, selectedNodeService) {
+      $scope.options = {
+        autoResize: true
+      };
+      
+      $scope.data = {
+        "nodes": [
+          {id: 1, label: 'Node 1'},
+          {id: 2, label: 'Node 2'},
+          {id: 3, label: 'Node 3'},
+          {id: 4, label: 'Node 4'},
+          {id: 5, label: 'Node 5'}
+        ],
+        "edges": [
+          {from: 1, to: 3},
+          {from: 1, to: 2},
+          {from: 2, to: 4},
+          {from: 2, to: 5}
+        ]
+      };
+
+      $scope.events = {
+        onload: function(network) {
+          network.on('selectNode', function(node) {
+            selectedNodeService.setSelectedNode(node);
+          });
+
+          network.on('deselectNode', function(node) {
+            selectedNodeService.setSelectedNode({});
+          });
+        }
+      };
+    }])
     .run(function($rootScope) {
       // WebSocket service
 
@@ -17,28 +62,4 @@
         console.log(event.data);
       }
     });
-
-  GraphController.$inject['$scope', 'VisDataSet'];
-
-  function GraphController($scope, VisDataSet) {
-    $scope.options = {
-      autoResize: true
-    };
-    
-    $scope.data = {
-      "nodes": [
-        {id: 1, label: 'Node 1'},
-        {id: 2, label: 'Node 2'},
-        {id: 3, label: 'Node 3'},
-        {id: 4, label: 'Node 4'},
-        {id: 5, label: 'Node 5'}
-      ],
-      "edges": [
-        {from: 1, to: 3},
-        {from: 1, to: 2},
-        {from: 2, to: 4},
-        {from: 2, to: 5}
-      ]
-    };
-  }
 })();
