de96e5017395d83dbdc5d51a1b01dfb99f1f4ad5
Andrew Bowler <ajbowler@iastate.edu>
2015-11-17 12:52:43 -0600

    Add WebSocket to main Graphalyzer component

--- graph_ui/js/app.js
+++ graph_ui/js/app.js
@@ -7,11 +7,37 @@ var SearchPanel = require('./SearchPanel.js');
 var NodeInfoPanel = require('./NodeInfoPanel.js');
 
 var Graphalyzer = React.createClass({
+  getDefaultProps: function() {
+    return {
+      websocket: new WebSocket("ws://rwhite226.duckdns.org:1618/ws")
+    };
+  },
+
+  getInitialState: function() {
+    return {
+      graphData: {},
+      selectedNode: {}
+    };
+  },
+
+  componentDidMount: function() {
+    this.websocket.onmessage = handleWSMessage;
+  },
+
+  handleWSMessage: function(event) {
+    var response = event.data;
+    if (response !== null) {
+      this.setState({graphData: response});
+    }
+  },
+
   render: function() {
     return (
       <div>
         <GraphPanel />
-        <SearchPanel />
+        <SearchPanel 
+          websocket = {this.props.websocket}
+        />
         <NodeInfoPanel />
       </div>
     );
