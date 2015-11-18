afd603428be3bc62c60f95d73427f3825b29a018
Andrew Bowler <bowlerandrewj@gmail.com>
2015-11-17 23:16:23 -0600

    Live reload, so whenever you change your JS it automatically rebuilds the page for you!

--- graph_ui/gulpfile.js
+++ graph_ui/gulpfile.js
@@ -2,13 +2,35 @@ var gulp = require('gulp');
 var browserify = require('browserify');
 var babel = require('gulp-babel');
 var source = require('vinyl-source-stream');
+var livereload = require('gulp-livereload');
+var uglify = require('gulp-uglify');
+var streamify = require('gulp-streamify');
 
-gulp.task('default', function () {
+var files = {
+  js: ['./js/*.js'],
+};
+
+var paths = {
+  build: './build'
+};
+
+gulp.task('build', function () {
   browserify("js/app.js")
   .transform('babelify', {
     presets: ['es2015', 'react']
   })
   .bundle()
   .pipe(source('bundle.js'))
-  .pipe(gulp.dest('build'));
+  .pipe(streamify(uglify()))
+  .pipe(gulp.dest(paths.build))
+  .pipe(livereload());
 });
+
+// Rerun the task when a file changes
+gulp.task('watch', function() {
+  livereload.listen();
+  gulp.watch(files.js, ['build']);
+});
+
+// The default task (called when you run `gulp` from cli)
+gulp.task('default', ['watch', 'build']);
