7b7f219536fb26968c4397f7822cb9dc51f30552
Penguinparty <penguin_party@q.com>
2016-01-31 21:42:03 -0600

    Updated Neo4J upload file

--- graphalyzer-server/src/file_handler/neo4j/Neo4JInteraction.py
+++ graphalyzer-server/src/file_handler/neo4j/Neo4JInteraction.py
@@ -7,53 +7,55 @@ class Neo4JInteraction(object):
 		self.oSWrapper = oSWrapper
 
 	def loadEdges(self, graphlocation: str, graphid: str):
-		import py2neo
-		http.socket_timeout = sys.maxsize
+	    """Load edges cvs file into neo4j. graphlocation is string location of file
+	        and graphid is the unique id for this graph"""
+	    from py2neo import Graph
+	    from py2neo.packages.httpstream import http
+	    # Increase timeout because it can take a really long time.
+	    http.socket_timeout = sys.maxsize
 
-		print("graph location: " + graphlocation + " graphid: " + graphid)
-
-		fileURI = self.oSWrapper.getFileURI(graphlocation)
-
-		query = ("USING PERIODIC COMMIT LOAD CSV WITH HEADERS FROM 'file:" +
-		fileURI + "' AS line " +
-		"MERGE (child:node {id:line.childid, graphid:\"" +
-		graphid + "\"}) " +
-		"MERGE (parent:node {id:line.parentid, graphid:\"" +
-		graphid + "\"}) " +
-		"MERGE (child)-[edge:edge {id:line.edgeid, graphid:\"" +
-		graphid + "\"}]-(parent)")
-		print(query)
-		# noinspection PyBroadException
-		try:
-			graph = py2neo.Graph()
-			graph.cypher.execute(query)
-		except Exception:
-			print("Unable to connect to neo4j", sys.exc_info()[0])
-			traceback.print_exc(file=sys.stdout)
+	    query = ("USING PERIODIC COMMIT 1000 LOAD CSV  WITH HEADERS FROM 'file://" +
+	             graphlocation + "' AS line " +
+	             "MERGE (child:node {id:line.childid, graphid:\"" +
+	             graphid + "\"}) " +
+	             "MERGE (parent:node {id:line.parentid, graphid:\"" +
+	             graphid + "\"}) " +
+	             "MERGE (child)-[edge:edge {id:line.edgeid, graphid:\"" +
+	             graphid + "\"}]-(parent)")
+	    # noinspection PyBroadException
+	    try:
+	        graph = Graph()
+	        graph.cypher.execute(query)
+	    except Exception:
+	        print("Edges failed")
+	        print("Unable to connect to neo4j")
 
 	def loadProp(self, graphlocation: str, graphid: str):
-		# noinspection PyBroadException
+	    """Load properties cvs file into neo4j. graphlocation is string location of file
+	        and graphid is the unique id for this graph"""
+	    from py2neo.packages.httpstream import http
+	    # Increase timeout because it can take a really long time.
+	    http.socket_timeout = sys.maxsize
+	    # noinspection PyBroadException
+	    try:
+	        from py2neo import Graph
+	        import csv
+	        graph = Graph()
 
-		http.socket_timeout = sys.maxsize
-
-		try:
-			import py2neo
-			import csv
-			graph = py2neo.Graph()
-
-			iterator = iter(csv.reader(open(graphlocation)))
-			next(iterator)
-			count = 0
-			tx = graph.cypher.begin()
-			for row in iterator:
-				if count >= 500:
-					tx.process()
-					count = 0
-				query = ("MATCH (n { id:'" + row[0] + "', graphid:'" + graphid +
-				         "'}) SET n." + row[1] + " = '" + row[2] + "'")
-				tx.append(query)
-				count += 1
-			tx.commit()
-		except Exception:
-			print("Prop failed")
-			print("Unable to connect to neo4j")
+	        iterator = iter(csv.reader(open(graphlocation)))
+	        next(iterator)
+	        count = 0
+	        tx = graph.cypher.begin()
+	        # Loop each csv row so that the prop name can be included.
+	        for row in iterator:
+	            if count >= 1000:
+	                tx.process()
+	                count = 0
+	            query = ("MATCH (n { id:'" + row[0] + "', graphid:'" + graphid +
+	                     "'}) SET n." + row[1] + " = '" + row[2] + "'")
+	            tx.append(query)
+	            count += 1
+	        tx.commit()
+	    except Exception:
+	        print("Prop failed")
+	        print("Unable to connect to neo4j")
