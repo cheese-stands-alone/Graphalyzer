ef9059340a76eb592c312b65e8c22a68985ffccc
Andrew Bowler <bowlerandrewj@gmail.com>
2016-03-28 21:28:07 -0500

    Settings Modal triggered by button

--- graph_ui/js/Dashboard.js
+++ graph_ui/js/Dashboard.js
@@ -7,6 +7,8 @@
 'use strict';
 
 var React = require('react');
+var ReactBootstrap = require('react-bootstrap'),
+    Panel = ReactBootstrap.Panel;
 var Settings = require('./Settings.js');
 var NewSearchPanel = require('./NewSearchPanel.js');
 var ExportURL = require('./ExportURL.js');
@@ -14,9 +16,9 @@ var ExportURL = require('./ExportURL.js');
 var Dashboard = React.createClass({
   render: function() {
     return (
-      <Settings/>
-      <NewSearchPanel/>
-      <ExportURL/>
+      <Panel header='Dashboard' bsStyle='primary'>
+        <Settings />
+      </Panel>
     );
   }
 });
--- graph_ui/js/Settings.js
+++ graph_ui/js/Settings.js
@@ -7,16 +7,46 @@
 'use strict';
 
 var React = require('react');
+var ReactBootstrap = require('react-bootstrap'),
+    Button = ReactBootstrap.Button,
+    Modal = ReactBootstrap.Modal;
 var GraphLoader = require('./GraphLoader.js');
 var SubgraphPanel = require('./SubgraphPanel.js');
 var FilterPanel = require('./FilterPanel.js');
 
 var Settings = React.createClass({
+  getInitialState: function() {
+    return {
+      show: false
+    };
+  },
+
   render: function() {
+    let close = () => this.setState({ show: false});
+
     return (
-      <GraphLoader/>
-      <SubgraphPanel/>
-      <FilterPanel/>
+      <div className='modal-container'>
+        <Button 
+          bsStyle='success'
+          onClick={() => this.setState({ show: true})}
+          >Settings
+        </Button>
+        <Modal
+          show={this.state.show}
+          container={this}
+          aria-labelledby='settings-title'
+        >
+          <Modal.Header>
+            <Modal.Title id='settings-title'>Settings</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <p>One fine ass body...</p>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button bsStyle='primary' onClick={close}>Close</Button>
+          </Modal.Footer>
+        </Modal>
+      </div>
     );
   }
 });
